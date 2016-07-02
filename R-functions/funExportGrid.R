ec8cb11a4c216c500614abf7c8c76b552f48daf9
Nick Bearman <nick.bearman@gmail.com>
2016-07-02 15:46:13 +0100

    Add code to allow funExportGrid to be run manually

--- R-functions/funExportGrid.R
+++ R-functions/funExportGrid.R
@@ -1,26 +1,31 @@
-#function to export data as ASCII grid
-ExportGrid <- function(grid_m_smooth_pop, filename_prefix, grid_r_ID, columnName){
+#function to export data as ASCII grid, with option to calc percentages
+  #code to run to setup variable as they are called by analysis-no-overlay.R
+    columnName <- columnNames[i] 
+
   #replace NA with -1 (NA value for ascii grid)
-  grid_m_smooth_pop[which(is.na(grid_m_ID))] <- "-1"
-#export as ascii grid
-filename <- paste0("output/5a_ascii_grid",filename_prefix,"_",columnName,".asc")
-#filename <- "20151006-test-b.asc"
-#rows and cols are inversed for asc grid - see help (?as.raster) and http://stackoverflow.com/questions/14513480/convert-matrix-to-raster-in-r
-cat(paste0("ncols        ",nrow(grid_m_smooth_pop)), file = filename, sep = "\n")
-cat(paste0("nrows        ",ncol(grid_m_smooth_pop)), file = filename, sep = "\n", append = TRUE)
-#get xll and yll from grid data we read in
-cat(paste0("xllcorner    ",grid_r_ID@bbox[1,1]), file = filename, sep = "\n", append = TRUE)
-cat(paste0("yllcorner    ",grid_r_ID@bbox[2,1]), file = filename, sep = "\n", append = TRUE) 
-cat(paste0("cellsize     ",grid_r_ID@grid@cellsize[1]),  file = filename, sep = "\n", append = TRUE) 
-#ignore NODATA value
-cat(paste0("NODATA_value -1"), file = filename, sep = "\n", append = TRUE) 
-#output data
-#for each row (col in R)
-for (i in 1:ncol(grid_m_smooth_pop)) {
-  cat(grid_m_smooth_pop[,i], "\n", file = filename, append = TRUE)
-  #next i
-  i <- i + 1
-}
+    grid_m_smooth_pop[which(is.na(grid_m_ID))] <- "-1"
+  #export as ascii grid
+    filename <- paste0("output/5a_ascii_grid",filename_prefix,"_",columnName,".asc")
+    #filename <- "20151006-test-b.asc"
+  #rows and cols are inversed for asc grid - see help (?as.raster) and http://stackoverflow.com/questions/14513480/convert-matrix-to-raster-in-r
+    cat(paste0("ncols        ",nrow(grid_m_smooth_pop)), file = filename, sep = "\n")
+    cat(paste0("nrows        ",ncol(grid_m_smooth_pop)), file = filename, sep = "\n", append = TRUE)
+  #get xll and yll from grid data we read in
+    cat(paste0("xllcorner    ",grid_r_ID@bbox[1,1]), file = filename, sep = "\n", append = TRUE)
+    cat(paste0("yllcorner    ",grid_r_ID@bbox[2,1]), file = filename, sep = "\n", append = TRUE) 
+    cat(paste0("cellsize     ",grid_r_ID@grid@cellsize[1]),  file = filename, sep = "\n", append = TRUE) 
+  #ignore NODATA value
+    cat(paste0("NODATA_value -1"), file = filename, sep = "\n", append = TRUE) 
+  #output data
+  #for each row (col in R)
+    for (i in 1:ncol(grid_m_smooth_pop)) {
+      #notes #this section is very slow on a windows machine, but very fast on OSX. 
+      cat(grid_m_smooth_pop[,i], "\n", file = filename, append = TRUE)
+      #next i
+        i <- i + 1
+      #print i (for testing speed)
+        #print(i)
+    }
 
 #export lookup table as CSV
   #extract which grid cells
