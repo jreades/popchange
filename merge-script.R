909ffa790163202b149abbc6aa33251296342506
Nick Bearman <nick@nickbearman.me.uk>
2016-10-19 13:44:02 +0100

    Create script to merge CSV files

---
+++ merge-script.R
@@ -0,0 +1,23 @@
+#Script to merge multiple CSV files together
+
+#Setup new working directory
+  #get working directory
+    currentWorkingDirectory <- getwd()
+  #set to /merge
+    setwd(paste0(currentWorkingDirectory,"/merge"))
+#Get list of filenames to be merged
+  filenames <- list.files(path = ".") 
+#Merge files
+  tmp <- do.call("cbind", lapply(filenames, read.csv, header = TRUE)) 
+#remove gird_ID cols
+  tmp2 <- subset(tmp, select=-c(grid_ID))
+  tmp2 <- tmp[ , -which(names(tmp) %in% c("grid_ID"))]
+#apply names based on filenames
+  colnames(tmp2) <- filenames
+#export
+  #setup filename
+    filename <- paste0("output.csv")
+  #write CSV file
+    write.csv(tmp2, filename, row.names = FALSE)
+#Reset working directory
+  setwd(currentWorkingDirectory)
